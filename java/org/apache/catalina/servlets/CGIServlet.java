0c683af073211100f9826f73ee9b874b408fe341
Jonathan Gallimore <jon@jrg.me.uk>
2019-04-25 11:59:12 +0100

    Applying https://github.com/apache/tomcat/commit/7f0221b904956359f2d739aa3a2b53f8c12ed8c7

--- java/org/apache/catalina/servlets/CGIServlet.java
+++ java/org/apache/catalina/servlets/CGIServlet.java
@@ -52,6 +52,7 @@ import javax.servlet.http.HttpSession;
 import org.apache.catalina.util.IOTools;
 import org.apache.juli.logging.Log;
 import org.apache.juli.logging.LogFactory;
+import org.apache.naming.resources.JrePlatform;
 import org.apache.tomcat.util.res.StringManager;
 
 
@@ -247,6 +248,17 @@ public final class CGIServlet extends HttpServlet {
     /* some vars below copied from Craig R. McClanahan's InvokerServlet */
 
     private static final long serialVersionUID = 1L;
+    private static final Pattern DEFAULT_CMD_LINE_ARGUMENTS_DECODED_PATTERN;
+    private static final String ALLOW_ANY_PATTERN = ".*";
+
+    static {
+         if (JrePlatform.IS_WINDOWS) {
+             DEFAULT_CMD_LINE_ARGUMENTS_DECODED_PATTERN = Pattern.compile("[a-zA-Z0-9\\Q-_.\\/:\\E]+");
+         } else {
+             // No restrictions
+             DEFAULT_CMD_LINE_ARGUMENTS_DECODED_PATTERN = null;
+         }
+     }
 
     /**
      * The debugging detail level for this servlet. Useful values range from 0
@@ -305,6 +317,14 @@ public final class CGIServlet extends HttpServlet {
      */
     private Pattern cmdLineArgumentsEncodedPattern =
             Pattern.compile("[a-zA-Z0-9\\Q%;/?:@&,$-_.!~*'()\\E]+");
+
+    /**
+     * Limits the decoded form of individual command line arguments. Default
+     * varies by platform.
+     */
+    private Pattern cmdLineArgumentsDecodedPattern = DEFAULT_CMD_LINE_ARGUMENTS_DECODED_PATTERN;
+
+
 
     /**
      * Sets instance variables.
@@ -373,6 +393,13 @@ public final class CGIServlet extends HttpServlet {
                     Pattern.compile(getServletConfig().getInitParameter("cmdLineArgumentsEncoded"));
         }
 
+        String value = getServletConfig().getInitParameter("cmdLineArgumentsDecoded");
+        if (ALLOW_ANY_PATTERN.equals(value)) {
+            // Optimisation for case where anything is allowed
+            cmdLineArgumentsDecodedPattern = null;
+        } else if (value != null) {
+            cmdLineArgumentsDecodedPattern = Pattern.compile(value);
+        }
     }
 
 
@@ -850,7 +877,17 @@ public final class CGIServlet extends HttpServlet {
                             }
                             return false;
                         }
+
                         String decodedArgument = URLDecoder.decode(encodedArgument, parameterEncoding);
+                        if (cmdLineArgumentsDecodedPattern != null &&
+                                !cmdLineArgumentsDecodedPattern.matcher(decodedArgument).matches()) {
+                            if (log.isDebugEnabled()) {
+                                log.debug(sm.getString("cgiServlet.invalidArgumentDecoded",
+                                        decodedArgument, cmdLineArgumentsDecodedPattern.toString()));
+                            }
+                            return false;
+                        }
+
                         cmdLineParameters.add(decodedArgument);
                     }
                 }
@@ -1173,7 +1210,6 @@ public final class CGIServlet extends HttpServlet {
             this.env = envp;
 
             return true;
-
         }
 
         /**
